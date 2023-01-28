8ade0e379de39d13d459a41f31cd7d1209b14a7b
Yuriy Nikitin <yuriy.nikitin@splzone.com>
2023-01-28 22:20:48 +0300

    Done Main args

---
+++ Chapter3-OOP/MainArgs/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace MainArgs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Переменные среды из параметров Main");
+            foreach (var arg in args)
+            {
+                Console.WriteLine(arg);
+            }
+            Console.ReadKey();
+
+            Console.WriteLine("Переменные среды из Environment");
+            foreach (var envArg in Environment.GetCommandLineArgs())
+            {
+                Console.WriteLine(envArg);
+            }
+            Console.ReadKey();
+        }
+    }
+}
