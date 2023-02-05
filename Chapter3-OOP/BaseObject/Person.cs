be7e21fce563f108d7b764ac1516d9660ad4b9d5
Yuriy Nikitin <yuriy.nikitin@splzone.com>
2023-02-05 21:31:20 +0300

    3.8. Наследование от класса Oblect 3.9. Переопредение методов 3.10. Обращение к предку из класса

---
+++ Chapter3-OOP/BaseObject/Person.cs
@@ -0,0 +1,36 @@
+﻿namespace BaseObject
+{
+    internal class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public Person(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName}";
+        }
+
+        public new bool Equals(object? obj)
+        {
+            return obj is Person person
+                && FirstName == person.FirstName
+                && LastName == person.LastName;
+        }
+
+        public override int GetHashCode()
+        {
+            Console.WriteLine($"Базовый хешкод: {base.GetHashCode()}");
+
+            var h = new HashCode();
+            h.Add(FirstName);
+            h.Add(LastName);
+            return h.ToHashCode();
+        }
+    }
+}
---
+++ Chapter3-OOP/BaseObject/Program.cs
@@ -0,0 +1,38 @@
+﻿using BaseObject;
+
+Console.WriteLine("Переопредение ToString()");
+Person person = new Person("Юрий", "Никитин");
+Console.WriteLine(person.ToString());
+
+object obj = person;
+Console.WriteLine(obj.ToString());
+
+
+Person p1 = new("Юрий", "Никитин");
+Person p2 = new("Юрий", "Никитин");
+Person p3 = p1;
+
+Console.WriteLine("\nПереопределение Equals()");
+Console.WriteLine(p1.Equals(p2));
+Console.WriteLine(p1.Equals(p3));
+
+Console.WriteLine("\nСравнение по значению и по ссылке");
+Console.WriteLine(ComparePersons(p1, p2));
+Console.WriteLine(ComparePersons(p1, p3));
+
+Console.WriteLine("\nПереопредение GetHashCode()");
+Console.WriteLine(p1.GetHashCode());
+Console.WriteLine(p2.GetHashCode());
+Console.WriteLine(p3.GetHashCode());
+
+
+string ComparePersons(Person person1, Person person2)
+{
+    bool equalData = person1.Equals(person2);
+    // Т.к. метод Equals переопределен через new - можем получить к нему доступ через родителя
+    bool equalRefs = ((object)person1).Equals(person2);
+
+    if (equalRefs) return "Ссылки объектов одинаковые";
+    if (equalData) return "Данные объектов одинаковые";
+    return "Объекты разные";
+}
