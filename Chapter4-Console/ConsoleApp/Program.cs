ccb29b0703f24066859a20fc508243f1a94c60a4
Yuriy Nikitin <yuriy.nikitin@splzone.com>
2023-02-08 23:58:37 +0300

    4.0 Консольные приложения

---
+++ Chapter4-Console/ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Title = "Радужная консолька";
+
+foreach (var color in Enum.GetValues<ConsoleColor>())
+{
+    // Расчитываем левый отступ - делим ширину на 2, чтобы получить середину
+    Console.CursorLeft = (Console.BufferWidth - color.ToString().Length) / 2;
+    Console.CursorTop = 10; // Отступаем сверху
+    Console.ForegroundColor = color; // Устанавливаем цвет шрифта
+    Console.WriteLine(color); // Выводим строковое представление цвета
+    Task.Delay(TimeSpan.FromSeconds(1)).Wait(); // Задержка
+    Console.Clear(); // Очистка
+}
