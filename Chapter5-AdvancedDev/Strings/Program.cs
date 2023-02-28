9f8d5c991c4558a3597d7da2a7a64e5a2894b5be
Yuriy Nikitin <yuriy.nikitin@splzone.com>
2023-02-28 20:12:51 +0300

    5.6. Класс строк

---
+++ Chapter5-AdvancedDev/Strings/Program.cs
@@ -0,0 +1,29 @@
+﻿
+string sample = "Stop war!";
+
+Console.WriteLine($"Строка содержит \"war\" = {sample.Contains("war")}");
+
+Console.WriteLine(string.Format("Мощность машины {0} л.с.", 115));
+
+Console.WriteLine($"Ищем начало \"war\". Индекс начала = {sample.IndexOf("war")} ");
+
+Console.WriteLine(sample.Insert(8, " on Donbass"));
+
+Console.WriteLine(sample.PadLeft(sample.Length + 5));
+
+Console.WriteLine(sample.Remove(4, 4));
+
+Console.WriteLine(sample.Replace("war", "USA"));
+
+Console.WriteLine(sample.ToUpper());
+
+Console.WriteLine($"Don't {sample.Substring(0, 4).ToLower()} me now");
+
+
+Console.WriteLine();
+Console.WriteLine("C:\\Windows\\system32\\filename.txt");
+Console.WriteLine(@"C:\\Windows\\system32\\filename.txt");
+Console.WriteLine(@"Multiline 
+                        string");
+Console.WriteLine("Введите строку:");
+Console.WriteLine($"Строка пуста = {string.IsNullOrEmpty(Console.ReadLine())}");
