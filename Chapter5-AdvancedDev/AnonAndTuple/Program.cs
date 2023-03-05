a03a9e290350db7e95886efe4650d034213bb9f7
Yuriy Nikitin <yuriy.nikitin@splzone.com>
2023-03-05 15:16:54 +0300

    5.9. Анонимные типы. 5.10. Кортежи

---
+++ Chapter5-AdvancedDev/AnonAndTuple/Program.cs
@@ -0,0 +1,36 @@
+﻿
+// Переменная анонимного типа
+var person = new
+{
+    FirstName = "Yura",
+    LastName = "Nik",
+    Age = 25
+};
+
+// Свойства анонимного типа readonly
+//person.Age = 34;
+
+Console.WriteLine($"Чел: Имя - {person.FirstName} Фамилия - {person.LastName} Возраст - {person.Age} \n");
+
+// System.Tuple не то же самое, что кортежи самого языка, они преобразуются в System.ValueTuple
+// System.Tuple - класс с (readonly) свойствами
+// System.ValueTuple - структура с полями
+// https://blog.christian-schou.dk/ultimate-guide-to-use-tuple-in-programming/
+// Tuple<double, double> coordinates = (34.78, 23.34); // Ошибка
+// ValueTuple<double, double> coordinates = (34.78, 23.34); // Норм
+
+// Декомпозиция кортежа
+var (latitude, longitude) = GetCoordinates();
+
+PrintCootdinates((latitude, longitude));
+
+(double, double) GetCoordinates()
+{
+    return (25.34, 18.93);
+}
+
+// Использование именованного кортежа
+void PrintCootdinates((double latitude, double longitude) coordinates)
+{
+    Console.WriteLine($"Широта: {coordinates.latitude} Долгота: {coordinates.longitude}");
+}
