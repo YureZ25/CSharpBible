bbe69faa2aa9a2fcf99ca68ff8c5d128b800b844
Yuriy Nikitin <yuriy.nikitin@splzone.com>
2023-03-06 21:09:49 +0300

    8.2. Исключения в C#

---
+++ Chapter8-Exceptions/Exception/Program.cs
@@ -0,0 +1,23 @@
+﻿
+while (true)
+{
+    Console.WriteLine("Введите число (q чтобы выйти)");
+
+    var input = Console.ReadLine();
+
+    if (input == "q") break;
+
+    try
+    {
+        var i = Convert.ToInt32(input);
+        Console.WriteLine($"Вы ввели {i}");
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine($"Вы ввели не корректное число '{input}'");
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e.Message);
+    }
+}
