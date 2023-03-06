1a1bffc98efa36b450f24c434bdae576cb0210b1
Yuriy Nikitin <yuriy.nikitin@splzone.com>
2023-03-06 16:41:47 +0300

    7.1. Базовый класс для массивов

---
+++ Chapter7-Collections/BaseArray/Program.cs
@@ -0,0 +1,31 @@
+﻿
+int[] test = { 10, 20, 1, 6, 15 };
+
+Console.WriteLine("Исходный массив: ");
+Print(test);
+
+Console.WriteLine("Отсортированный: ");
+Array.Sort(test);
+Print(test);
+
+Console.WriteLine("Реверсированный: ");
+Array.Reverse(test);
+Print(test);
+
+Console.WriteLine("Очистка нескольких элементов:");
+Array.Clear(test, 2, 2);
+Print(test);
+
+Console.WriteLine("Изменение размера:");
+Array.Resize(ref test, 3);
+Print(test);
+
+Console.WriteLine("Бинарный поиск:");
+int foundIndex = Array.BinarySearch(test, 15);
+Console.WriteLine($"Индекс искомого элемента = {foundIndex}");
+
+void Print(int[] array)
+{
+    foreach (int i in array) Console.Write(i + " ");
+    Console.WriteLine();
+}
