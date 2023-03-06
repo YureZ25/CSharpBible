b64478409d5c87e3c4db5447f516be05c1a12e9d
Yuriy Nikitin <yuriy.nikitin@splzone.com>
2023-03-06 21:33:38 +0300

    8.9. Переполнение

---
+++ Chapter8-Exceptions/Overflow/Program.cs
@@ -0,0 +1,20 @@
+﻿
+int x = 1000000;
+int y = 3000;
+int z = 0;
+
+try
+{
+    z = checked(x * y);
+
+    checked
+    {
+        z = x * y;
+    }
+}
+catch (OverflowException)
+{
+    Console.WriteLine("Значение вышло за пределы");
+}
+
+Console.WriteLine($"Значение: {z}");
