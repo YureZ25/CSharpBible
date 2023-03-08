befbe5795d4f00a8fc3dae74dcfdfd67797cf0bc
Yuriy Nikitin <yuriy.nikitin@splzone.com>
2023-03-08 17:12:45 +0300

    10.1. LINQ при работе с массивами 10.1.1. SQL-стиль использования LINQ 10.1.2. Использование LINQ через методы 10.2. Магия IEnumerable 10.3. Доступ к данных

---
+++ Chapter10-LINQ/Linq/Person.cs
@@ -0,0 +1,33 @@
+﻿namespace Linq
+{
+    internal class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+        public Person(string firstName, string lastName, int age)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+        }
+
+        public static List<Person> GetPeople()
+        {
+            return new List<Person>
+            {
+                new("Юра", "Никитин", 25),
+                new("Таня", "Прокопенко", 20),
+                new("Марк", "Васильев", 27),
+                new("Никита", "Дубенко", 26),
+                new("Андрей", "Чернышев", 28),
+            };
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} - {Age}";
+        }
+    }
+}
---
+++ Chapter10-LINQ/Linq/Program.cs
@@ -0,0 +1,55 @@
+﻿using Linq;
+
+int ageContition;
+var people = Person.GetPeople();
+
+// SQL и лямбда синтаксис
+ageContition = 26;
+var results = from p in people
+              where p.Age > ageContition
+              select new { p.FirstName, p.Age };
+
+Console.WriteLine($"Люди старше {ageContition}");
+foreach (var p in results)
+{
+    Console.WriteLine($"Имя: {p.FirstName}; Возраст - {p.Age};");
+}
+
+Console.WriteLine($"\nЛюди младше {ageContition}");
+foreach (var p in people.Where(p => p.Age < ageContition))
+{
+    Console.WriteLine($"Имя: {p.FirstName}; Возраст - {p.Age};");
+}
+
+// Магия IEnumerable
+var filtered = people.Where(p => p.Age < ageContition);
+
+Console.WriteLine($"\n\nЛюди младше {ageContition}");
+foreach (var p in filtered)
+{
+    Console.WriteLine(p);
+}
+
+var leo = new Person("Лёня", "Никитин", 19);
+Console.WriteLine($"\nДобавим человека: {leo}");
+people.Add(leo);
+
+Console.WriteLine($"\nЛюди младше {ageContition}");
+foreach (var p in filtered)
+{
+    Console.WriteLine(p);
+}
+
+// Доступ к данным
+var filterFirst = "Юра";
+if (people.Any(p => p.FirstName == filterFirst))
+{
+    var yura = people.First(p => p.FirstName == filterFirst);
+    Console.WriteLine($"\nПервый по условию: {yura}");
+}
+
+Console.WriteLine("\nПервые 2 после 3х");
+foreach (var p in people.Skip(2).Take(3))
+{
+    Console.WriteLine(p);
+}
