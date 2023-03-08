e210fe4e2a7ca2eaead29bea05e62d40890dc406
Yuriy Nikitin <yuriy.nikitin@splzone.com>
2023-03-08 14:53:02 +0300

    9.1. Делегаты 9.2. События и их вызов

---
+++ Chapter9-Events/Delegates/Person.cs
@@ -0,0 +1,26 @@
+﻿namespace Delegates
+{
+    internal class Person
+    {
+        private int _age;
+        public string Name { get; set; }
+        public int Age
+        {
+            get => _age;
+            set
+            {
+                if (value < 0) throw new ArgumentException("Возраст не может быть отрицательным");
+                _age = value;
+                if (AgeChanged != null) AgeChanged(this, EventArgs.Empty); // или AgeChanged?.Invoke(this, EventArgs.Empty)
+            }
+        }
+
+        public event EventHandler? AgeChanged;
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+}
---
+++ Chapter9-Events/Delegates/Program.cs
@@ -0,0 +1,17 @@
+﻿using Delegates;
+
+Person me = new Person("Yura", 25);
+
+me.AgeChanged += ShowAgeChanged;
+me.AgeChanged += new EventHandler(ShowAgeChanged);
+
+me.Age++;
+
+
+void ShowAgeChanged(object? sender, EventArgs e)
+{
+    if (sender is Person person)
+    {
+        Console.WriteLine($"Возраст {person.Name} изменился на {person.Age}");
+    }
+}
