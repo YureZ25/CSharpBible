ff3e01fee4cb03cf3cc3822f779a8105583b67f0
Yuriy Nikitin <yuriy.nikitin@splzone.com>
2023-03-08 17:28:47 +0300

    10.4. LINQ для доступа к XML

---
+++ Chapter10-LINQ/LinqXml/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Xml.Linq;
+
+var root = XElement.Load("./../../../person.xml");
+
+Console.WriteLine("Спосок людей старше 16");
+foreach (var item in root
+    .Elements("person")
+    .Where(e => Convert.ToInt32(e.Attribute("age")?.Value) < 16))
+{
+    Console.WriteLine(item.Attribute("FirstName")?.Value);
+}
+
+Console.WriteLine("\nСпосок людей живущих в Ростове");
+foreach (var item in root
+    .Elements("person")
+    .Where(e => e.Elements("address").Elements("city").First().Value == "Ростов"))
+{
+    Console.WriteLine(item.Attribute("FirstName")?.Value);
+}
