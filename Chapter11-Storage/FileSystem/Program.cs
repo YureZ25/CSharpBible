8c60c4bb34bf029aad562e19c5f7270308cadadc
Yuriy Nikitin <yuriy.nikitin@splzone.com>
2023-03-19 23:17:46 +0300

    11.1. Файловая система 11.2. Текстовые файлы

---
+++ Chapter11-Storage/FileSystem/Program.cs
@@ -0,0 +1,25 @@
+﻿
+GetFiles("C:\\Users\\YureZ");
+
+void GetFiles(string path)
+{
+	Console.WriteLine($"Папки и файлы по пути '{path}':");
+
+	// Берем полные пути ко всем директориям по указанному пути 
+	foreach (string dir in Directory.GetDirectories(path))
+	{
+		// Пропускаем скрытые файлы с помощью побитового И
+		if ((File.GetAttributes(dir) & FileAttributes.Hidden) == FileAttributes.Hidden) continue;
+
+		// Берем только название, отбрасывая путь
+        string dirName = Path.GetFileName(dir);
+		Console.WriteLine(dirName?.ToUpperInvariant());
+	}
+
+	// То же самое, только для файлов
+	foreach (string file in Directory.GetFiles(path))
+	{
+        string fileName = Path.GetFileName(file);
+		Console.WriteLine(fileName?.ToLowerInvariant());
+	}
+}
---
+++ Chapter11-Storage/TextFiles/Program.cs
@@ -0,0 +1,53 @@
+﻿
+Console.WriteLine("Введите название файла для создания/открытия:");
+var fileName = Console.ReadLine();
+
+var list = GetListFromFile(fileName, out var path);
+
+Console.WriteLine("Содержимое файла:");
+foreach (var item in list)
+{
+    Console.WriteLine(item);
+}
+
+Console.WriteLine("Добавте данные. Для выхода введите q");
+while (true)
+{
+    var input = Console.ReadLine();
+    if (input == "q") break;
+    list.Add(input);
+}
+
+Console.WriteLine("Сохоанить данные? (y/n)");
+if (Console.ReadLine() == "y")
+{
+    SaveListToFile(path, list);
+}
+
+IList<string> GetListFromFile(string? fileName, out string path)
+{
+    if (string.IsNullOrEmpty(fileName)) throw new ArgumentException("Имя файла не должно быть пустым");
+
+    path = Path.Combine(Directory.GetCurrentDirectory(), fileName + ".list");
+
+    if (File.Exists(path))
+    {
+        return File.ReadAllLines(path).ToList();
+    }
+    else
+    {
+        return new List<string>();
+    }
+}
+
+void SaveListToFile(string path, IList<string> list)
+{
+    using (var sw = File.CreateText(path))
+    {
+        foreach (var item in list)
+        {
+            sw.WriteLine(item);
+        }
+        sw.Close();
+    }
+}
