6e1a1ae600c485ca6d6b68411a9977f821c0c100
Yuriy Nikitin <yuriy.nikitin@splzone.com>
2023-03-25 11:59:38 +0300

    11.5. Потоки MemoryStream

---
+++ Chapter11-Storage/MemoryFile/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text;
+
+const string example = "Эта строка была помещена и изьята из оперативной памяти";
+
+var strBytes = Encoding.Unicode.GetBytes(example);
+var strBytesCount = Encoding.Unicode.GetByteCount(example);
+
+var ms = new MemoryStream(strBytesCount);
+
+ms.Write(strBytes, 0, strBytesCount);
+
+var readBuffer = new byte[strBytesCount];
+ms.Seek(0, SeekOrigin.Begin);
+ms.Read(readBuffer, 0, strBytesCount);
+
+Console.WriteLine(Encoding.Unicode.GetString(readBuffer));
