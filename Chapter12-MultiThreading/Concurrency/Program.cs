6c6214e3092cf4df8357d5bb3d7a73eb3939402b
Yuriy Nikitin <yuriy.nikitin@splzone.com>
2023-04-15 18:58:22 +0300

    12.3. Конкурентный доступ

---
+++ Chapter12-MultiThreading/Concurrency/Program.cs
@@ -0,0 +1,23 @@
+﻿
+object syncObj = new(); // Объект синхронизации
+
+for (int i = 0; i < 5; i++)
+{
+    var t = new Thread(() => ThreadFunc()) // Создаем поток с именем
+    {
+        Name = $"Поток {i}"
+    };
+    t.Start(); // Запускаем
+}
+
+void ThreadFunc()
+{
+    lock (syncObj) // Lock секция гарантирует выполнение только этого кода в моменте
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine($"{Thread.CurrentThread.Name} - {i}");
+            Thread.Sleep(100);
+        }
+    }
+}
