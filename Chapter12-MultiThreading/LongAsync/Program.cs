2f6ae893b7cb6c4ad5ff3ed0ac550d80f7e8aafc
Yuriy Nikitin <yuriy.nikitin@splzone.com>
2023-04-16 22:01:59 +0300

    Долгие задачи из книги

---
+++ Chapter12-MultiThreading/LongAsync/Program.cs
@@ -0,0 +1,22 @@
+﻿
+int factorialToCalculate = 11;
+
+var factorialTask = Task.Run(() =>
+{
+    int result = 1;
+	for (int i = 1; i < factorialToCalculate; i++)
+	{
+		result *= i;
+		Task.Delay(10).Wait();
+	}
+	return result;
+});
+
+while (factorialTask.Status != TaskStatus.RanToCompletion)
+{
+    Console.WriteLine($"Статус - {factorialTask.Status}");
+	Task.Delay(100).Wait();
+}
+
+Console.WriteLine($"Статус - {factorialTask.Status}");
+Console.WriteLine($"Результат = {factorialTask.Result}");
