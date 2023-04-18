0bb70aaa2b3229cdb6f519ba357e0d365e91c7bb
Yuriy Nikitin <yuriy.nikitin@splzone.com>
2023-04-18 21:58:32 +0300

    13.7. Представления 13.8. Модель представлений

---
+++ Chapter13-Web/Views/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Views.ViewModels;
+
+namespace Views.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            // В качестве параметра можно указать название представления
+            // Фреймворк пытается его найти в "Views/{controllerName}/{actionName}.cshtml",
+            // а потом в "Views/Shared/{actionName}.cshtml"
+            return View("Main");
+
+            // Или путь, если они храниятся не по стандартной схеме, описанной выше
+            // Он пишется относительно предполагаемого места
+            //return View("../../ViewModels/Main");
+        }
+
+        public IActionResult Person()
+        {
+            var me = new Person("Yura", "Nikitin", 25);
+
+            // Метод также принимает произвольный объект модели представления
+            // Представление исчется по стандартной схеме
+            return View(me);
+        }
+    }
+}
---
+++ Chapter13-Web/Views/ViewModels/Person.cs
@@ -0,0 +1,19 @@
+﻿namespace Views.ViewModels
+{
+    // Модели предствалений - это классы с данными (без логики, максимум валидационная)
+    // Для передачи представлениям или из них в приложение (аналог DTO в API)
+
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+        public Person(string firstName, string lastName, int age)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+        }
+    }
+}
