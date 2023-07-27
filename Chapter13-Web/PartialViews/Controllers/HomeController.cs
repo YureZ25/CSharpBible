18378a0874b588f6cc8c7263516eac8f150fbe27
Yuriy Nikitin <iyura1997@gmail.com>
2023-07-27 12:11:40 +0300

    13.10.3. Встраиваемые представления

---
+++ Chapter13-Web/PartialViews/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PartialViews.ViewModels;
+
+namespace PartialViews.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult List()
+        {
+            var list = new Person[]
+            {
+                new("Yura", "Nikitin", 25),
+                new("Tanya", "Prokopenko", 21),
+                new("Mikhail", "Flenov", 42),
+                new("Ivan", "Sergeev", 29),
+                new("Lena", "Petrova", 19),
+            };
+
+            return View(list);
+        }
+
+        public IActionResult Details()
+        {
+            return View(new Person("Yura", "Nikitin", 25));
+        }
+    }
+}
