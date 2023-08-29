855a58f2c6883b7bcc511700f6d1fcac6d6b8ea2
Yuriy Nikitin <yuriy.nikitin@splzone.com>
2023-08-30 00:13:34 +0300

    14.6. Память

---
+++ Chapter14-ManagedCode/Memory/Program.cs
@@ -0,0 +1,83 @@
+﻿using System.Runtime.InteropServices;
+
+int[] srcArray = { 10, 20, 5, 2, 54, 9 };
+
+foreach (var i in srcArray)
+{
+    Console.WriteLine($"Исходное значение: {i}");
+}
+
+Console.WriteLine("\nВыделение памяти в стеке:");
+
+unsafe
+{
+    // Выделяем память в стеке для массива из n int элементов
+    // stackalloc возвращает указатель на начало выделенной области
+    int* tripleArray = stackalloc int[srcArray.Length];
+
+    for (int i = 0; i < srcArray.Length; i++)
+    {
+        // Благодаря арифметике указателей (элементы массива стоят в памяти по порядку) мы можем обращатся к ним
+        if (i < srcArray.Length / 2)
+        {
+            tripleArray[i] = 3 * srcArray[i]; // по индексу
+        }
+        else
+        {
+            *(tripleArray + i) = 3 * srcArray[i]; // или используя арифметику указателей на прямую
+        }
+
+        Console.WriteLine($"Значение: {tripleArray[i]}");
+    }
+
+    // Как и для управляемого когда, память выделенная stackalloc освободится при выходе из метода
+}
+
+Console.WriteLine("\nПолучение указателя на управляемый объект:");
+
+Point point = new Point(3, 17);
+
+var handle = GCHandle.Alloc(point);
+
+var pointAddr = GCHandle.ToIntPtr(handle);
+
+unsafe
+{
+    var pointPtr = (Point*)pointAddr.ToPointer();
+
+    Console.WriteLine($"Произвдение координат: {pointPtr->x * pointPtr->y}");
+}
+
+handle.Free();
+
+Console.WriteLine("\nФиксирование объекта в куче:");
+
+unsafe
+{
+    fixed (int* powTwo = srcArray)
+    {
+        for (int i = 0; i < srcArray.Length; i++)
+        {
+            powTwo[i] = (int)Math.Pow(*(powTwo + i), 2); 
+        }
+    }
+}
+
+foreach (var i in srcArray)
+{
+    Console.WriteLine($"Типо исходное значение: {i}");
+}
+
+public class Point
+{
+    public int x;
+    public int y;
+
+    public Point(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+
--- Chapter14-ManagedCode/UnsafeCode/Program.cs
+++ Chapter14-ManagedCode/UnsafeCode/Program.cs
@@ -9,6 +9,8 @@ unsafe
     Console.WriteLine($"Значение по указанному адресу: {*ptr}"); // Для получения данных - разименовываем (*)
 
     ptr++; // Инкремент указателя - сдвиг в памяти на размер типа в байтах (здесь плюс 2)
+    Console.WriteLine($"Размер типа {x.GetType()} равен {sizeof(short)}");
+    Console.WriteLine($"Размер указателя типа {x.GetType()} равен {sizeof(short*)}"); // Здесь значение будет зависеть от битности системы
 
     Console.WriteLine($"Адрес: {(int)ptr}");
     Console.WriteLine($"Значение по указанному адресу: {*ptr}");
@@ -18,15 +20,13 @@ Point point = new Point { X = 3, Y = 29 };
 
 unsafe
 {
-#pragma warning disable CS8500 // This takes the address of, gets the size of, or declares a pointer to a managed type
-    Point* ptr = &point; // Так делать не желательно
-#pragma warning restore CS8500 // This takes the address of, gets the size of, or declares a pointer to a managed type
+    Point* ptr = &point; // В стеке можно спокойно делать дела с указателями
 
     Console.WriteLine($"Адрес: {(int)ptr}");
     Console.WriteLine($"Значение по указанному адресу: {ptr->ToString()}"); // Вызываем метод по указателю (->)
 }
 
-class Point
+struct Point
 {
     public int X { get; set; }
     public int Y { get; set; }
